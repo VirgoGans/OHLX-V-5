ba5d9f100131fc6305c0787c72156a17a634f227
VirgoGans <77391671+VirgoGans@users.noreply.github.com>
2022-10-01 16:58:12 +0700

    Add files via upload

--- plugins/group-revoke.js
+++ plugins/group-revoke.js
@@ -1,13 +1,21 @@
-let handler = async (m, { conn }) => { 
-   let res = await conn.groupRevokeInvite(m.chat) 
-   let gruf = m.chat 
-   conn.reply(m.sender, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gruf), m) 
- } 
- handler.help = ['revoke'] 
- handler.tags = ['group'] 
- handler.command = /^re(voke|new)(invite|link)?$/i 
- handler.group = true 
- handler.admin = true 
- handler.botAdmin = true 
-  
- export default handler
+let handler = async (m, { isAdmin, isOwner, conn, command }) => {
+  if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
+  conn.groupRevokeInvite(m.chat)
+  conn.reply(m.chat, `Sukses ${command} link grup, link telah di kirim ke chat pribadi`, m)
+  await delay(1000)
+  let linknya = await conn.groupInviteCode(m.chat)
+  conn.reply(m.sender, 'https://chat.whatsapp.com/' + linknya, m)
+}
+handler.help = ['revoke']
+handler.tags = ['group']
+handler.command = /^re(voke|new|set)(invite|link)?$/i
+
+handler.group = true
+handler.botAdmin = true
+
+module.exports = handler
+
+const delay = time => new Promise(res => setTimeout(res, time))
